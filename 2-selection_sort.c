002c2202423b6529f325985bcd795f937e4d743f
Richard Gwati <haggiog0844@gmail.com>
2023-06-27 17:40:49 +0200

    feat: sorting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,7 +25,7 @@ void selection_sort(int *array, size_t size)
 		if (min_idx != i)
 		{
 			int temp = array[i];
-		
+
 			array[i] = array[min_idx];
 			array[min_idx] = temp;
 			print_array(array, size);
