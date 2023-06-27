1edade707400ab2c106e0e79faed540edf0b3bcb
Richard Gwati <haggiog0844@gmail.com>
2023-06-27 15:28:04 +0200

    feat: bubble sort betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,11 +8,12 @@
   */
 void bubble_sort(int *array, size_t size)
 {
-size_t i,j;
+size_t i, j;
 
 if (array == NULL || size == 0)
-		return;
-
+{
+return;
+}
 for (i = 0; i < size - 1; i++)
 {
 for (j = 0; j < size - i - 1; j++)
